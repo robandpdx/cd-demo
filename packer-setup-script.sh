0c6c33f26de92eb75e0650164fadf2484f1c48b8
Rob Anderson <robandpdx@github.com>
2023-04-18 16:43:06 -0600

    do this stuff with sudo

--- packer-setup-script.sh
+++ packer-setup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>Hello World!</h1>" > /var/www/html/index.html
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo sh -c 'echo "<h1>Hello World!</h1>" > /var/www/html/index.html'
